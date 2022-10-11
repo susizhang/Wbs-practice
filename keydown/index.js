5e5e36061fd393e1a59ec99a6d8f5db46b460d39
suqun zhang <susizhang2017@gmail.com>
2022-10-11 17:09:15 +0200

    Adding keydown practice

---
+++ keydown/index.js
@@ -0,0 +1,9 @@
+const input = document.querySelector("#inputId");
+
+input.addEventListener("keydown", function (e) {
+  // console.log(e);
+  console.log(e.target.value);
+  // console.log(e.target.value + e.key);
+});
+
+// console.log(input);
